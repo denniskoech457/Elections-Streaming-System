a1228e8c9e88f3f7fff72397fc15738a2cad6b01
denniskoech457 <73883136+denniskoech457@users.noreply.github.com>
2022-04-14 18:35:16 +0300

    changes fixed
    changes fixed

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,5 @@
 <?php
   // Create database connection
   
-  $db = mysqli_connect("sql201.epizy.com", "epiz_30816694", "GpG6xNZxVe927l", "epiz_30816694_elections");
+  $db = mysqli_connect("RS2.NOC254.COM", "gorgeous_elections", "elections8445", "gorgeous_elections");
 ?>
